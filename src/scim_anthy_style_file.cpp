994ce9fd866e6b698ee505bf857bd4d35c0cf9b9
Takuro Ashie <makeinu>
2005-07-07 06:57:01 +0000

    typo.

--- src/scim_anthy_style_file.cpp
+++ src/scim_anthy_style_file.cpp
@@ -30,7 +30,7 @@ escape (const String &str)
 
     for (unsigned int i = 0; i < dest.size (); i++) {
         if (dest[i] == '#'  ||                   // for comment
-            dest[i] == '\\' ||                   // for backslash it self
+            dest[i] == '\\' ||                   // for backslash itself
             dest[i] == '='  ||                   // for separatort
             dest[i] == '['  || dest[i] == ']' || // for section
             dest[i] == '\t' || dest[i] == ',')   // for array
